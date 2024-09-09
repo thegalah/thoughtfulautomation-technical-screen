935850063c737cc13d14368db57815b2a50a1b9f
Matthew <thegalah@gmail.com>
2024-09-09 15:58:05 -0700

    Test cases

---
+++ typescript-node-service/src/sort.ts
@@ -0,0 +1,7 @@
+import { Stack } from "./Stack";
+
+const BULKY_THRESHOLD_VOLUME_CUBIC_CM = 10e6;
+const BULKY_DIMENSION_THRESHOLD_CM = 150;
+const HEAVY_THRESHOLD_KG = 20;
+
+export const sort = (widthCm: number, heightCm: number, lengthCm: number, massKg: number): Stack => {};
